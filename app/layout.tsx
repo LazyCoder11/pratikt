da8ba8017220117a485f5e0e530d4bbe1cfbc702
Pratik Trivedi <pratiktrivedi719@gmail.com>
2025-10-29 13:24:32 +0530

    Added Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "@/components/Navbar";
 import LenisScroll from "@/components/global/ScrollWrapper";
 import { Analytics } from "@vercel/analytics/react";
 import { BackgroundMusic } from "@/components/global/BackgroundMusic";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Pratik Trivedi — Web Designer",
@@ -24,6 +25,22 @@ export default function RootLayout({
           async
           src="https://r2.leadsy.ai/tag.js?pid=UT8quN98W4X9oAdL&version=062024"
         ></script>
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-KWLZQDCTT8"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+        >
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-KWLZQDCTT8');
+          `}
+        </Script>
       </head>
       <body className={`${Neue.variable} font-neueMontreal`}>
         <BackgroundMusic />
